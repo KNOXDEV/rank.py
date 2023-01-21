f04fa609a41d432372be7f81729da83bf57b0399
Nick Knox <nick@knox.codes>
2023-01-20 16:21:12 -0800

    readme and usage

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # rank.py
-computer-aided human-powered sorting 
+
+This is a no-dependency python script that allows humans to interactively
+sort small-to-medium sized lists at maximum theoretical efficiency. 
+
+For example, for an unaided human to properly sort a list of 150 items, in the worst case they will need 
+up to 22,500 comparisons. **This utility will allow you to sort the same list with
+no greater than 1,085 comparisons, a ~95% savings**!
+
+## examples
+
+1. ranking political candidates for a ranked choice election.
+2. organizing a movie collection by how much you enjoyed each entry.
+3. creating a dumb, uninspired tier list of something for clout with friends.
+
+...and many more!
+
+## usage
+
+```bash
+python rank.py states.txt > sorted-output.txt
+```
+
+```yaml
+# interactive output example
+Which is 'greater'? 
+ (1) - 'Alaska'          or 
+ (2) - 'Arizona'
+Which is 'greater'? 
+ (1) - 'Alabama'         or 
+ (2) - 'Alaska'
+# etc...
+```
+
+## approach
+
+Generally, when humans are asked to rank a set of items, they tend to use a 
+[hybrid insertion sort](https://en.wikipedia.org/wiki/Insertion_sort) approach.
+The problem with this approach is that it scales very poorly.
+
+The sorting algorithm that this utility uses is a custom depth-first [merge sort](https://en.wikipedia.org/wiki/Merge_sort)
+with certain properties that are conducive to human sorting, namely that in a vast majority of comparisons, 
+**only one of the items being compared will change at a time**, which greatly reduces the cognitive load for human sorting.
+
--- rank.py
+++ rank.py
@@ -1,85 +1,81 @@
 import sys
 
-# def swap(arr, i, j):
-# 	k = arr[i]
-# 	arr[i] = arr[j]
-# 	arr[j] = k
-
-
-# def sort(arr, start, end):
-# 	length = end - start + 1
-# 	if length == 1 or length == 0:
-# 		return
-# 	if length == 2:
-# 		if compare(arr[start], arr[end]):
-# 			swap(arr, start, end)
-# 		return
-# 	# otherwise, pick a pivot and start (left hand point by convention)
-# 	pivot = arr[start]
-# 	left = start + 1
-# 	right = end
-
-# 	# sort based on the pivot
-# 	while left <= right:
-# 		if compare(arr[right], pivot):
-# 			right -= 1
-# 			continue
-# 		if compare(pivot, arr[left]):
-# 			left += 1
-# 			continue
-# 		swap(arr, left, right)
-# 		left -= 1
-# 		right -= 1
-
-# 	# put pivot in the middle
-# 	swap(arr, start, right)
-
-# 	# sort left and right sub arrays
-# 	sort(arr, start, right - 1)
-# 	sort(arr, right + 1, end)
 
 # given two sorted lists, return a new list of all sorted elements
 def merge(a, b):
-	c = []
-	i = 0
-	j = 0
-	while i < len(a) and j < len(b):
-		if not compare(a[i], b[j]):
-			c.append(a[i])
-			i += 1
-		else:
-			c.append(b[j])
-			j += 1
-	while i < len(a):
-		c.append(a[i])
-		i += 1
-	while j < len(b):
-		c.append(b[j])
-		j += 1
-	return c 
+    c = []
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b):
+        if not compare(a[i], b[j]):
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+    while i < len(a):
+        c.append(a[i])
+        i += 1
+    while j < len(b):
+        c.append(b[j])
+        j += 1
+    return c
+
 
 def mergesort(arr):
-	if len(arr) == 1 or len(arr) == 0:
-		return arr
-	if len(arr) == 2:
-		if compare(arr[0], arr[1]):
-			return [arr[1], arr[0]]
-		else:
-			return arr
-	mid = len(arr) // 2
-	a = mergesort(arr[:mid])
-	b = mergesort(arr[mid:])
-	return merge(a, b)
+    if len(arr) == 1 or len(arr) == 0:
+        return arr
+    if len(arr) == 2:
+        if compare(arr[0], arr[1]):
+            return [arr[1], arr[0]]
+        else:
+            return arr
+    mid = len(arr) // 2
+    a = mergesort(arr[:mid])
+    b = mergesort(arr[mid:])
+    return merge(a, b)
 
 
 # true if a >= b, false otherwise
 def compare(a, b):
-	print(f"Which is greater, '{a}' (a), or '{b}' (b) ?")
-	answer = input("a/b: ") 
-	return answer == 'b'
+    answer = '0'
+    while answer not in ['1', '2']:
+        print(f"Which is 'greater'? \n (1) - '{a}' \t or \n (2) - '{b}'", file=sys.stderr)
+        answer = read_char()
+
+    return answer == '2'
 
 
-with open(sys.argv[1]) as f:
-	lines = f.read().splitlines()
+# cross-platform reads a single character in raw mode
+def read_char():
+    # try the Windows method, fall back to Unix
+    try:
+        import msvcrt
+        ch = msvcrt.getch()
+    except ImportError:
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if ord(ch) == 3:
+        print("Ctrl+C detected, aborting")
+        exit(0)
+    return ch
 
-print(mergesort(lines))
+
+# if this is being executed from the command line
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} INPUT_FILE", file=sys.stderr)
+        exit(1)
+
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
+
+    sorted_items = mergesort(lines)
+    for item in sorted_items:
+        print(item)
